12ab9a5fd789ef7ea86c546f31dbf88ec927b3c0
kimberlyas <kimberly.soares02@gmail.com>
2015-11-18 00:34:30 -0500

    Updated db config for user accounts

--- userdbConfig.php
+++ userdbConfig.php
@@ -2,16 +2,16 @@
 
 define ("DB_HOST", "localhost"); // set database host
 
-define ("DB_USER", "Admin"); // set database user
+define ("DB_USER", ""); // set MySQL database user
 
-define ("DB_PASS","passXDword"); // set database password
+define ("DB_PASS",""); // set MySQL database password
 
-define ("DB_NAME","UserAccountDB"); // set database name
+define ("DB_NAME","useraccountdb"); // set database name
 
 
-
+// Connect to server
 $link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");
-
+// Select database
 $db = mysql_select_db(DB_NAME, $link) or die("Couldn't select database");
 
 ?>
