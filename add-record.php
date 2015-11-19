2bfdb54f87e9b5905df9ef294fa424b9910352e9
shan_g <ir_5hangee@hotmail.com>
2015-11-19 12:49:09 +0000

    Updated add-record.php file added

--- add-record.php
+++ add-record.php
@@ -1,4 +1,6 @@
 <?php
+    include "access.php";
+
     if (isset($_POST['Save'])) {
          $fname = $_POST['fname'];
        	 $trn = $_POST['trn'];
@@ -12,7 +14,7 @@
          $dob = $_POST['dob'];
          $occupation = $_POST['occupation'];
          $cnumber = $_POST['cnumber'];
-         $sql = "insert into VictimInfo (lName, mName, fName, trn, mStatus, address, email, occupation, dob, hNumber, wNumber, cNumber) values ($lname, $mname, $fname, $trn, $mStatus, $address, $email, $occupation, $dob, $hnumber, $wnumber, $cnumber, )";
+         $sql = "INSERT into VictimInfo (trn, lname_acc, fname_acc, mname, address1_acc, address2_acc, email, home, cell, work, dob, occupation, mortalStatus) values ($trn, $lname, $fname, $mname, $address, $email, $hnumber, $cnumber, $wnumber, $dob, $occupation, $mStatus)";
    		 $result = mysql_query($sql,$conn) or die(mysql_error());
     }
 ?>
