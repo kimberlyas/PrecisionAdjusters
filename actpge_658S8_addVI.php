a63ef969a6edfaa0da5a8c53b55310660e1b74bd
Shanice A. Bryan <0shanAbry1@users.noreply.github.com>
2015-11-19 20:02:17 -0500

    Update actpge_658S8_addVI.php
    - Push data sent to the server from the form to the back-end database Precision_Adjusters

--- actpge_658S8_addVI.php
+++ actpge_658S8_addVI.php
@@ -1,13 +1,23 @@
-<?php include "logon.php";
-    if($_POST['addVI_Save']){
+<?php 
+    include "logon.php";
+	include dbaccess.php;    
+
+	if(isset($_POST['addVI_Save'])){
+		$vin = $_POST['vin']; //Vehicle Identification Number <br> <input type = "text" name = "vin"> <br><br>
         $plateNo = $_POST['plateNo']; //Plate Number <br> <input type = "text" name = "plateNo"> <br><br>
-        $vin = $_POST['vin']; //Vehicle Identification Number <br> <input type = "text" name = "vin"> <br><br>
-        $model = $_POST['model']; //Model <br> <input type = "text" name = "model"> <br><br>
-        $make = $_POST['make']; //Make <br> <input type = "text" name = "make"> <br><br>
-        $type = $_POST['type']; //Type <br> <input type = "text" name = "type"> <br><br>
+       	$type = $_POST['type']; //Type <br> <input type = "text" name = "type"> <br><br>        
+		$make = $_POST['make']; //Make <br> <input type = "text" name = "make"> <br><br>
+		$model = $_POST['model']; //Model <br> <input type = "text" name = "model"> <br><br>        
         $year = $_POST['year']; //Year <br> <input type = "text" name = "year"> <br><br>
         $damages = $_POST['damages']; //Damage Description <br> <input type = "textarea" name = "damages"> <br><br>
         
-        
+        //INSERT INTO table_name (column1, column2, column3) VALUES ('value1', 'value2', 'value3');
+		$dbqVI = "INSERT INTO vehicle_accinfo (vin,plateNo,type,make,model,year,damages) VALUES ($vin,$plateNo,$type,$make,$model,$year,$damages)";
+		$query = mysql_query($dbqVI);
+
+		if ($query === false) {
+            	echo "Could not successfully run query ($dbqVI) from DB ($database): " . mysql_error();
+            	exit;
+        }
     }
 ?>
