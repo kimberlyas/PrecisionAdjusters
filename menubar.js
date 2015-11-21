4da667effb6c19362a9ab22077ad0002621be082
Shanice A. Bryan <indi.shanbry@gmail.com>
2015-11-21 09:49:06 +0000

    Official html & css template

---
+++ menubar.js
@@ -0,0 +1,21 @@
+var drop;
+
+window.onload = function() {
+	var a = document.getElementById('label');
+	console.log(a);
+	a.onclick = function(){
+		drop = document.getElementById('nav-wrap');
+		if(drop.style.overflow === "hidden") {
+			drop.style.overflow = 'visible';
+			a.style.background = '#fff';
+			a.style.color = '#333';
+			console.log('clicked');
+		}
+		else {
+			drop.style.overflow = 'hidden';
+			a.style.background = 'transparent';
+			a.style.color = '#fff';
+			console.log('unclicked');
+		}
+	}
+}
