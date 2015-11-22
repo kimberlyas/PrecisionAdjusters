7127628129a8ca6d1782c14673a5667cdc4565a3
Shanice A. Bryan <indi.shanbry@gmail.com>
2015-11-22 10:39:14 +0000

    Minor tweeks before transferal of form tags to correct html/css

--- dbaccess.php
+++ dbaccess.php
@@ -2,7 +2,7 @@
 	$host = 'localhost';
 	$user = 'root';
 	$password = '$passXDword07$';
-	$database = 'Precision_Adjusters';
+	$database = 'PA_Database';
 	$connection = mysql_connect($host,$user,$password);
 	mysql_select_db($databse);
 ?>
