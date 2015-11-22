3092e478ba91c1ea834d1ab3d589d5054733b078
Kimberly Soares <kimberly.soares02@gmail.com>
2015-11-22 16:27:19 -0500

    Main Menu Screen for Database Manager
    Updates: Added HOME button to Menu nav bar
    
    Need to fix button layout and add links to buttons.

--- adminMenu.php
+++ adminMenu.php
@@ -1,65 +1,131 @@
-<?php include "logon.php";
-
-session_start(); // Start Session
-
+<?php  
+	include "userdbConfig.php"; // Connection to useraccountdb and myPHPadmin login credentials
+	session_start(); 
 ?>
 
-<!DOCTYPE HTML>
 <!-- Menu Screen for Precision Adjusters Website (Database Manager) -->
+
+<!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
 	<title>Precision Adjusters</title>
-	<link rel="stylesheet" href="stylesheet-admin.css" type="text/css">
+	<link rel="stylesheet" type="text/css" href="page_basic.css" />
+	<link rel="stylesheet" type="text/css" href="stylesheet-button.css" />
+	<script type="text/javascript" src="menubar.js"></script>
 </head>
 <body>
-	<div id="header">
-		<div class="section">
-			<div class="logo">
-				<a href="index.html">Precision Adjusters</a>
+	<div class="page">
+		<div class="header">
+			<div class="topbar">
+				<label id="label">
+					<span>
+						<span>Menu</span>
+					</span>
+				</label>
+				<div id="logo"> 
+					<span>
+						<span><img src="images/Company Logo.png" style="padding-top: 10px"></img></span>
+
+					</span>
+				</div>
 			</div>
-			<ul>
-				<li>
-					<a><?php if ($_SESSION['userType']) : ?>
-						<?php echo $_SESSION['userType']; ?>
-						<?php endif; ?>
-					</a> <!--UserType --> 
-				</li>
-				<li>
-					<a><?php if ($_SESSION['username']) : ?>
-						<?php echo $_SESSION['username']; ?>
-						<?php endif; ?>
-					</a> <!--UserName --> 
-				</li>
-				<li>
-					<a href="logon.php?id=0">Logout
-						<?php if ($_GET['id'] == 0) {
-						if(isset($_SESSION['username']))
-							unset($_SESSION['username']);
-						if(isset($_SESSION['password']))
-							unset($_SESSION['password']);
-						if(isset($_SESSION['loggedIn']))
-							unset($_SESSION['loggedIn']);
-						session_destroy();
-					}?> </a>  <!--Logout --> 
-				</li>
-			</ul>
 		</div>
-	</div>
-	<div id="body">
-			<div>
-				<h1>Main Menu</h1>
+        <!--End of top bar-->
+		<div class="banner-wrap">
+			<div class="container">
+				<div class="banner">
+					<span>MAIN MENU</span>
+				</div>
 			</div>
-			<div>
-				<a href='addNewUser.php' class='button' id='button1'>Add New User</a>
-				<a href='#' class='button' id='button2'>Add and Update Records</a>
-				<a href='#' class='button' id='button3'>Seach and View Records</a>
+		</div>
+		<!-- End of header -->
+		<div id="nav-wrap" style="overflow: hidden">
+			<div class="container">
+				<div class="desktop-nav">
+					<ul>
+						<li>
+							<a href="
+							<?php 
+								if (isset($_SESSION["userType"]))
+								{
+									// Link to different menu page based on user type
+         							if ($_SESSION["userType"] == 'Database Manager'){
+         								$homeLink = 'adminMenu.php';
+         							}
+         							elseif ($_SESSION["userType"] == 'Field Investigator') {
+         								$homeLink = 'FImenu.php';
+         							}
+         							elseif ($_SESSION["userType"] == 'Customer Service Agent') {
+         								$homeLink = 'csAgentMenu.php';
+         							}
+
+         							echo $homeLink;
+
+								}
+							?>">
+
+								Home
+
+					 		</a>  <!--Home (Redirects to respesctive Main Menu screen based on logged in User's type) -->
+						</li>	
+						<li> <!--UserType -->
+							<?php if (isset($_SESSION["userType"])) : ?>
+							<?php  echo $_SESSION["userType"]; ?>
+							<?php endif; ?>
+						</li>	 
+						<li> <!--UserName -->
+							<?php if (isset($_SESSION["username"])) : ?>
+							<?php  echo $_SESSION["username"]; ?>
+							<?php endif; ?>
+					    </li>		
+						<li> <!--Logout --> 
+							<a href="logon.php?id=0">Logout
+							<?php 
+								if (isset($_SESSION["loggedIn"]))
+								{
+									// Update database
+         							$mysql = "UPDATE userAccount SET loggedIn=0 WHERE user = '".$_SESSION["username"]."' ";
+         							$result = mysql_query($mysql,$link);
+
+         							//Check that it was updated
+         							if (!$result)
+         							{
+         								echo "Could not successfully run query ($sql) from DB: " . mysql_error();
+         							}
+
+         							// Unset all session variables
+									session_destroy();
+								}
+							?>
+					 		</a>  
+						</li>	
+					</ul>
+				</div>
 			</div>
-	<div>
-	<div id="footer">
-			<p>
-				&copy; Copyright 2015 | Precision Adjusters | All rights reserved.
-			</p>
+		</div>
+		<!-- End of Nav Bar -->
+		<div class="main-wrap">
+			<div class="container">
+				<div class="pos-wrap">
+					<table style="margin: 0px auto;" class="buttons" border="0"> 
+			
+							<tr style="align:centre margin-top:120px;;"><td><a href='addNewUser.php' class='button' id='button1'>Add New User</a></td></tr>
+							<tr style="align:centre;"><td><a href='#' class='button' id='button2'>Add and Update Records</a></td></tr>
+							<tr style="align:centre;"><td><a href='#' class='button' id='button3'>Seach and View Records</a></td></tr>
+
+					</table>
+						
+				</div>
+			</div>
+		</div>
+		<!-- End of main wrap -->
+		<div class="footer-wrap">
+			<div class="container">
+				<div class="footer">
+					<span>&copy; Copyright 2015 | Precision Adjusters | All rights reserved</span>
+				</div>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
