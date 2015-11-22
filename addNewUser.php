8780542813d43d115680be78894f8206d7c1abc4
Kimberly Soares <kimberly.soares02@gmail.com>
2015-11-22 16:34:29 -0500

    Add New User Screen (Database Manager)
    Updates: Added HOME button to Menu nav bar (Returns user to Database Manager main menu screen)
    
    Need to: Fix form layout, Fix functionality: User type

--- addNewUser.php
+++ addNewUser.php
@@ -1,22 +1,25 @@
-
-<?php include "logon.php";
+<?php  include "userdbConfig.php"; // Connection to useraccountdb and myPHPadmin login credentials
 
 session_start(); // Start Session
+//session_id('PrecisionAdjusters'); //SET id first before calling  session start
 
-if ($_POST['newUserSubmit']) {
-	$firstName = $_POST['firstName'];
-	$lastName = $_POST['lastName'];
-	$password = $_POST['password'];
-	$password2 = $_POST['password2'];
-	$userType = $_POST['userType'];
-	$errors = array();
+$msg = "";
+$errors = array();
+// Submit button
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get entered field data
+	$firstName = $_POST["firstName"];
+	$lastName = $_POST["lastName"];
+	$password = $_POST["password"];
+	$password2 = $_POST["password2"];
+	$userType = $_POST["userType"];
 
 	//Check passwords match
 	if ($password != $password2) {
 		$errors[] = "Entered passwords do not match";
 	}
 
-	//Check first name
+	/*Check first name
 	if(empty($firstName)){
 		$errors[] = "First name required";
 	}
@@ -32,16 +35,16 @@ if ($_POST['newUserSubmit']) {
 	}
 
 	//Check user type
-	if(!isset($_POST['userTypeSelect'])){
+	if(!isset($_POST["userTypeSelect"])){
 		$errors[] = "User Type required";
-	}
+	}*/
 
 	//Create username for new user
-	$username = $firstName + $lastName;
+	$username .= $firstName . $lastName;
 
 	//Check to see if username has been used before
-	$sql = "SELECT * FROM  userAccount WHERE username = '$username'";
-        $query = mysql_query($sql);
+	$sql = "SELECT * FROM  userAccount WHERE user = '$username'";
+    $query = mysql_query($sql);
 
         if ($query === false) {
             echo "Could not successfully run query ($sql) from DB: " . mysql_error();
@@ -51,12 +54,10 @@ if ($_POST['newUserSubmit']) {
         if (mysql_num_rows($query) > 0) { //If they match
          	
          	//Append number
-         	$username = $username + mysql_num_rows($query);
+         	$username .= mysql_num_rows($query);
             
         }
         
-    }
-
     //If there are no errors, proceed with addition to database
     if (empty($errors)) {
     	// Encrypt password
@@ -64,122 +65,164 @@ if ($_POST['newUserSubmit']) {
     	// Add values
     	$mysql = "INSERT INTO userAccount (user,pass,userType) VALUES ('$username', '$encPassword','$userType')"; 
     	$querry = mysql_query($mysql);
-    }
-    else{// If there are erros, output them as list items
-    	echo "<ul>";
-    	foreach ($errors as $error){
-    		echo "<li>".$error."</li>";
-    	}
-    	echo "</ul>";
-    }
-    
-}
+    	// Check if values were indeed added
+    	if (!$querry)
+         {
+         	echo "Could not successfully run query ($sql) from DB: " . mysql_error();
+         }
+         else
+         {
+         	$msg = "New user ".$username." successfully added";
+         }
 
+    }
+   
+}
+    
 ?>
 
 <!DOCTYPE HTML>
-<!-- Add New User Screen for Precision Adjusters Website (Database Manager)-->
+
+<!-- Add User Screen for Precision Adjusters Website (Database Manager) -->
+
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Precision Adjusters</title>
-	<link rel="stylesheet" href="stylesheet-adduser.css" type="text/css">
+	<link rel="stylesheet" type="text/css" href="page_basic.css" /> 
+	<link rel="stylesheet" type="text/css" href="stylesheet-form.css" /> 
+	<script type="text/javascript" src="menubar.js"></script>
 </head>
 <body>
-	<div id="header">
-		<div class="section">
-			<div class="logo">
-				<a href="index.html">Precision Adjusters</a>
+	<div class="page">
+		<div class="header">
+			<div class="topbar">
+				<label id="label">
+					<span>
+						<span>Menu</span>
+					</span>
+				</label>
+				<div id="logo"> 
+					<span>
+						<span><img src="images/Company Logo.png" style="padding-top: 10px"></img></span>
+					</span>
+				</div>
+			</div><!--End of topbar-->
+		</div><!--End of header-->
+		
+		<div class="banner-wrap">
+			<div class="container">
+				<div class="banner">
+					<br><br><span>ADD NEW USER</span> <!--Title of Current Interface>-->
+				</div>
 			</div>
-			<ul>
-				<li>
-					<a><?php if ($_SESSION['userType']) : ?>
-						<?php echo $_SESSION['userType']; ?>
-						<?php endif; ?>
-					</a> <!--UserType --> 
-				</li>
-				<li>
-					<a><?php if ($_SESSION['username']) : ?>
-						<?php echo $_SESSION['username']; ?>
-						<?php endif; ?>
-					</a> <!--UserName --> 
-				</li>
-				<li>
-					<a href="logon.php?id=0">Logout
-						<?php if ($_GET['id'] == 0) {
-						if(isset($_SESSION['username']))
-							unset($_SESSION['username']);
-						if(isset($_SESSION['password']))
-							unset($_SESSION['password']);
-						if(isset($_SESSION['loggedIn']))
-							unset($_SESSION['loggedIn']);
-						session_destroy();
-					}?> </a>  <!--Logout --> 
-				</li>
-			</ul>
-		</div>
-	</div>
-	<div id="body">
-			<div>
-				<h1></h1>
+		</div><!-- End of banner-wrap -->
+		
+		<div id="nav-wrap" style="overflow: hidden">
+			<div class="container">
+				<div class="desktop-nav">
+					<ul>
+						<li>
+							<a href="
+							<?php 
+								if (isset($_SESSION["userType"]))
+								{
+									// Link to different menu page based on user type
+         							if ($_SESSION["userType"] == 'Database Manager'){
+         								$homeLink = 'adminMenu.php';
+         							}
+         							elseif ($_SESSION["userType"] == 'Field Investigator') {
+         								$homeLink = 'FImenu.php';
+         							}
+         							elseif ($_SESSION["userType"] == 'Customer Service Agent') {
+         								$homeLink = 'csAgentMenu.php';
+         							}
+
+         							echo $homeLink;
+
+								}
+							?>">
+
+								Home
+
+					 		</a>  <!--Home (Redirects to respesctive Main Menu screen based on logged in User's type) -->
+						</li>	
+						<li>
+							<a>
+								<?php if (isset($_SESSION["userType"])) : ?>
+								<?php echo $_SESSION["userType"]; ?>
+								<?php endif; ?>
+							</a>
+						</li>	<!--User Type -->
+						<li>
+							<a><?php if (isset($_SESSION["username"])) : ?>
+							<?php echo $_SESSION["username"]; ?>
+							<?php endif; ?></a>
+						</li>  <!--User Name -->		
+						<li>
+							<a href="logon.php?id=0">Logout
+							<?php 
+								if (isset($_SESSION["loggedIn"]))
+								{
+									// Update database
+         							$mysql = "UPDATE userAccount SET loggedIn=0 WHERE user = '".$_SESSION["username"]."' ";
+         							$result = mysql_query($mysql,$link);
+
+         							//Check that it was updated
+         							if (!$result)
+         							{
+         								echo "Could not successfully run query ($sql) from DB: " . mysql_error();
+         							}
+
+         							// Unset all session variables
+									session_destroy();
+								}
+							?>
+					 		</a>  <!--Logout -->
+						</li>	
+					</ul>
+				</div>
 			</div>
-			<!--<img id="top" src="top.png" alt="">-->
-			<div id="form_container">
-	
-				<h1><a>Add new user</a></h1>
-				<form id="form_1072598" class="appnitro"  method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
-					<div class="form_description">
-							<h2>Add new user</h2>
-							<p></p>
-					</div>						
-							<ul >
-								<li id="li_1" >
-									<label class="description" for="element_1">Name </label>
-									<span>
-										<input id="firstName" name= "element_1_1" class="element text" maxlength="255" size="8" 
-										value="<?php if ($_POST['firstName']) {echo $_POST['firstName']}?>"/>
-										<label>First</label>
-									</span>
-									<span>
-										<input id="lastName" name= "element_1_2" class="element text" maxlength="255" size="14" 
-										value="<?php if ($_POST['lastName']) {echo $_POST['lastName']}?>"/>
-										<label>Last</label>
-									</span> 
-								</li>
-								<li id="li_2" >
-									<label class="description" for="element_2">Password </label>
-									<div>
-										<input id="password" name="element_2" class="element text medium" type="text" maxlength="255" value=""/> 
-									</div> 
-								</li>
-								<li id="li_3" >
-									<label class="description" for="element_3">Re-Type Password </label>
-									<div>
-										<input id="passsword2" name="element_3" class="element text medium" type="text" maxlength="255" value=""/> 
-									</div> 
-								</li>
-								<li id="li_4" >
-									<label class="description" for="element_4">User Type </label>
-									<div>
-										<select class="element select medium" id="userType" name="element_4"> 
-											<option value="1" selected="selected">Please select one</option>
-											<option value="2" >Database Manager</option>
-											<option value="3" >Field Investigator</option>
-											<option value="4" >Customer Service Agent</option>
-										</select>
-									</div> 
-								</li>
-								<li class="buttons">
-			    					<input type="hidden" name="form_id" value="1072598" />
-									<input id="newUserSubmit" class="button_text" type="submit" name="submit" value="Submit" />
-								</li>
-							</ul>
-			</form>	
-		<div>
-	<div id="footer">
-			<p>
-				&copy; Copyright 2015 | Precision Adjusters | All rights reserved.
-			</p>
+		</div><!-- End of nav-bar -->
+		
+		<div class="main-wrap">
+			<div class="container">
+				<div class="pos-wrap">
+
+					<form name="addUser" action="<?= $_SERVER['PHP_SELF'] ?>" method="post"  style="margin: 0px auto; align:center; position: inline-block;">
+					<div class="add" style="padding:10px;">
+						<div style="color:red;">
+						<?php // If there are errors, output them as list items
+    						echo "<ul>";
+    						foreach ($errors as $error){
+    							echo "<li>".$error."</li>";}
+    						echo "</ul>";
+    					?>
+    					</div> <!-- Error Message -->
+    					
+    					<label class="description" for="element_4">Name</label>
+						<input type="text" placeholder="First Name" name="firstName" required ><br>
+						<input type="text" placeholder="Last Name" name="lastName" required ><br>
+						<label class="description" for="element_4">Password</label>
+						<input type="password" placeholder="password" name="password" required><br>
+						<input type="password" placeholder="re-type password" name="password2" required><br>
+						<label class="description" for="element_4">User Type </label>
+						<select class="dropdown" id="userType" name="userType"> 
+								<option value="1" selected="selected">Please select one</option>
+								<option value="2">Database Manager</option>
+								<option value="3">Field Investigator</option>
+								<option value="4">Customer Service Agent</option>
+						</select>
+						<input type="submit" value="Add user">
+						<div style="color:blue;"><?php echo $msg;?></div> <!-- Confirmation Message -->
+					
+					</div>
+				</form>
+
+				</div>
+			</div>
+		</div><!-- End of main wrap -->
+		<div class="footer"><span>&copy; Copyright 2015 | Precision Adjusters | All rights reserved.</span></div>
 	</div>
 </body>
 </html>
